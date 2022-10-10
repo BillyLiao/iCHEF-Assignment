1a0a19c27ded53475e446aa9eecaaa8e22598012
BillyLiao <hohey670@gmail.com>
2022-10-10 11:59:36 +0800

    Initial Commit

---
+++ iCHEF-Assignment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iCHEF-Assignment
+//
+//  Created by 廖慶麟 on 2022/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCHEF-Assignment/iCHEF_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCHEF_AssignmentApp.swift
+//  iCHEF-Assignment
+//
+//  Created by 廖慶麟 on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct iCHEF_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
